43af1e009fc7e9c42c55908718fab0e1b6d06172
Pedro Alfonzo Condori Fernandez <pedro.acf.3@gmail.com>
2019-06-12 21:27:37 -0400

    first deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel new app
                 </div>
 
                 <div class="links">
