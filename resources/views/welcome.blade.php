1c038651e29d4855b7072f665eff3da363ecc304
juancgt <juan.jose.cgt@gmail.com>
2019-06-13 22:35:40 -0400

    Prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel ultra new app
+                    Laravel ultra new app modificado
                 </div>
 
                 <div class="links">
