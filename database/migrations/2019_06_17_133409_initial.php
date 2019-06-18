5775c43c9ac29b8f7f24d2e07e336012cd271482
Pedro Alfonzo Condori Fernandez <pedro.acf.3@gmail.com>
2019-06-17 22:52:58 -0400

    template

---
+++ database/migrations/2019_06_17_133409_initial.php
@@ -0,0 +1,115 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Initial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->string('shortname');
+            $table->integer('group')->default(1);
+            $table->timestamps();
+        });
+
+        Schema::create('users_courses', function(Blueprint $table){
+            $table->integer('user_id');
+            $table->integer('course_id');
+            $table->enum('role', ['student', 'professor'])->default('student');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::create('tests', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('description');
+            $table->integer('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->integer('minutes');
+            $table->datetime('start_at');
+            $table->datetime('end_at');
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('questions', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('description');
+            $table->integer('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+
+        Schema::create('answers', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('description');
+            $table->boolean('correct')->default(false);
+            $table->integer('question_id');
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
+
+        Schema::create('test_rules', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('test_id');
+            $table->integer('category_id');
+            $table->integer('num_questions');
+            $table->foreign('test_id')->references('id')->on('tests');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+
+        Schema::create('solutions', function(Blueprint $table){
+            $table->increments('id');
+            $table->decimal('total', 5, 2);
+            $table->datetime('end_at');
+            $table->integer('test_id');
+            $table->foreign('test_id')->references('id')->on('tests');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('user_answers', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('question_id');
+            $table->integer('answer_id');
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->foreign('answer_id')->references('id')->on('answers');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_answers');
+        Schema::drop('solutions');
+        Schema::drop('test_rules');
+        Schema::drop('answers');
+        Schema::drop('questions');
+        Schema::drop('categories');
+        Schema::drop('tests');
+        Schema::drop('users_courses');
+        Schema::drop('courses');
+    }
+}
