5ebc2afa378d2fbc11654da2df2eb29a8b172220
Pedro Alfonzo Condori Fernandez <pedro.acf.3@gmail.com>
2019-06-20 21:16:41 -0400

    login controller fixed

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,12 @@ Route::group(['middleware' => ['auth']], function(){
         return view('welcome');
     });
 
-    Route::get('/categories', 'CategoryController@index');
+    Route::get('categories', 'CategoryController@index')->name('categories.index');
+    Route::get('categories/create', 'CategoryController@create')->name('categories.create');
+    Route::get('categories/{id}', 'CategoryController@detail')->name('categories.detail');
+    Route::post('categories/create', 'CategoryController@store')->name('categories.store');
+    Route::get('questions/{category_id}/create', 'QuestionController@create')->name('questions.create');
+    Route::post('questions/{category_id}/create', 'QuestionController@store')->name('questions.store');
 
 
     Route::resource('courses','CourseController');
@@ -28,11 +33,3 @@ Route::group(['middleware' => ['auth']], function(){
 
     Route::get('/home', 'HomeController@index')->name('home');
 });
-
-Route::get('/categories', 'CategoryController@index')->name('categories.index');
-Route::get('/categories/create', 'CategoryController@create')->name('categories.create');
-Route::get('/categories/{id}', 'CategoryController@detail')->name('categories.detail');
-Route::post('/categories/create', 'CategoryController@store')->name('categories.store');
-Route::get('questions/{category_id}/create', 'QuestionController@create')->name('questions.create');
-Route::post('questions/{category_id}/create', 'QuestionController@store')->name('questions.store');
-
